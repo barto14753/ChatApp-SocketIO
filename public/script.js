509a678e46cb184bd5cb70850ed1c940c546b579
barto14753 <wlodarski_bartosz@o2.pl>
2021-09-02 22:50:24 +0200

    scroll to bottom

--- public/script.js
+++ public/script.js
@@ -147,7 +147,7 @@ function addRecievedMessage(message)
 
     // highlighting message-box
     let msg_box = $('.messages-box-list').find("#" + message.sender.id).find('.messages-box-username');
-    if (!msg_box.hasClass('font-weight-bold') && chat.hasClass('d-none'));
+    if (!msg_box.hasClass('font-weight-bold') && !chat.hasClass('current-box'));
     {
         msg_box.addClass('text-dark');
         msg_box.addClass('font-weight-bold');
