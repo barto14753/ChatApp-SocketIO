5b285406a6270dcb81af17ff192a9eed54305e15
barto14753 <wlodarski_bartosz@o2.pl>
2021-09-02 23:39:39 +0200

    scroll to bottom

--- public/script.js
+++ public/script.js
@@ -154,6 +154,7 @@ function addRecievedMessage(message)
     console.log(focused_user, message.sender.id, focused_user==null);
     if ((focused_user == null) || (focused_user.id != message.sender.id));
     {
+        console.log(focused_user.id != message.sender.id, focused_user.id !== message.sender.id)
         msg_user.addClass('text-dark');
         msg_user.addClass('font-weight-bold');
         msg_date.addClass('text-dark');
